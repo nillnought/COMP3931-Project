01a847c0f4a80a3834d5b19fc12c5ed4870915e2
nillnought <gglaim@my.bcit.ca>
2025-11-16 11:53:55 -0800

    Set up reading and writing to audio files

--- audioFile.py
+++ audioFile.py
@@ -2,5 +2,12 @@ import soundfile as sf
 import numpy as np
 class audioFile:
 
+
+# usage: audio = audioFile("filename.wav")
     def __init__(self, file_location):
-        fileAddress = file_location
+        self.file_address = file_location
+        self.audio_data, self.samp_freq = sf.read(file_address)
+
+# usage: audio.saveFile("newName.wav") can look into appending a file format when user chooses to save
+    def saveFile(self, new_file_name):
+        sf.write(new_file_name, self.audio_data, self.samp_freq)
