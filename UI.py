59cbc0f4161558aaf965227a202468155d674d2c
Kanon Nishiyama <knishiyama@my.bcit.ca>
2025-11-18 12:49:33 -0800

    Implement radio button filter selection

--- UI.py
+++ UI.py
@@ -2,7 +2,7 @@
 # https://www.geeksforgeeks.org/python/python-tkinter-tutorial/
 #for custom tkinter: https://customtkinter.tomschimansky.com/
 
-# import tkinter as tk
+import tkinter as tk
 import customtkinter as ctk
 from customtkinter import filedialog
 from audioFile import audioFile
@@ -30,61 +30,55 @@ class UI(ctk.CTk):
 
         toolbar_height = 150
 
-        self.files_frame = ctk.CTkFrame(self.toolbar_frame, height=toolbar_height)
-        self.files_frame.grid(row=0, column=0, sticky="nsew")
-        self.files_frame.grid_columnconfigure(0, weight=1)
-        self.files_frame.grid_rowconfigure((0, 1, 2, 3), weight=1)
-
-        file_button_color = "grey"
-        file_button_color_hover = "darkgreen"
-
-        self.close_program_button = ctk.CTkButton(self.files_frame, text="Close Program", command=self.on_close,
-                                           fg_color="darkred", hover_color="red")
-        self.close_program_button.grid(row=0, column=0, sticky="nsew")
-        self.import_button = ctk.CTkButton(self.files_frame, text="Import File", command=self.import_file,
-                                           fg_color=file_button_color, hover_color=file_button_color_hover)
-        self.import_button.grid(row=1, column=0, sticky="nsew")
-        self.export_button = ctk.CTkButton(self.files_frame, text="Export File", command=self.export_file,
-                                           fg_color=file_button_color, hover_color=file_button_color_hover)
-        self.export_button.grid(row=2, column=0, sticky="nsew")
-        self.close_button = ctk.CTkButton(self.files_frame, text="Close File", command=self.export_file,
-                                           fg_color=file_button_color, hover_color=file_button_color_hover)
-        self.close_button.grid(row=3, column=0, sticky="nsew")
-
-
-
-        self.controls_frame = ctk.CTkFrame(self.toolbar_frame, height=toolbar_height)
-        self.controls_frame.grid(row=0, column=1, sticky="nsew")
-        self.controls_frame.grid_columnconfigure((0, 1), weight=1)
-        self.controls_frame.grid_rowconfigure(0, weight=1)
-
-        control_button_width = 10
-        control_button_color = "grey"
-        control_button_color_hover = "darkgreen"
-
-
-        self.play_button = ctk.CTkButton(self.controls_frame, text="Play", command=self.play_audio, width=control_button_width,
-                                           fg_color=control_button_color, hover_color=control_button_color_hover)
-        self.play_button.grid(row=0, column=0, sticky="nsew")
-        self.pause_button = ctk.CTkButton(self.controls_frame, text="Pause", command=self.play_audio, width=control_button_width,
-                                           fg_color=control_button_color, hover_color=control_button_color_hover)
-        self.pause_button.grid(row=0, column=1, sticky="nsew")
+        self.files_frame = FilesFrame(self, toolbar_height)
+        self.controls_frame = ControlsFrame(self, toolbar_height)
 
 
 
         self.filters_frame = ctk.CTkFrame(self.toolbar_frame, fg_color="grey", height=toolbar_height)
-        self.filters_frame.grid(row=0, column=2, sticky="ew")
+        self.filters_frame.grid(row=0, column=2, sticky="nsew")
+        self.filters_frame.grid_columnconfigure((0, 1), weight=1)
+        self.filters_frame.grid_rowconfigure(0, weight=1)
 
+        self.select_filter_frame = ctk.CTkFrame(self.toolbar_frame, fg_color="grey", height=toolbar_height)
+        self.select_filter_frame.grid(row=0, column=2, sticky="nsew", padx = 15, pady = 5)
+        self.select_filter_frame.grid_columnconfigure(0, weight=1)
+        self.select_filter_frame.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
+
+        self.filter_select_var = tk.StringVar(value="")
+        def radiobutton_event():
+            print("Option toggled, current value:", self.filter_select_var.get())
+
+        self.select_reverb = ctk.CTkRadioButton(self.select_filter_frame, text="Reverb",
+                            command=radiobutton_event, variable=self.filter_select_var, value="reverb")
+        self.select_reverb.grid(row=0, column=0, sticky="nsew")
+        self.select_echo = ctk.CTkRadioButton(self.select_filter_frame, text="Echo",
+                            command=radiobutton_event, variable=self.filter_select_var, value="echo")
+        self.select_echo.grid(row=1, column=0, sticky="nsew")
+        self.select_lowpass = ctk.CTkRadioButton(self.select_filter_frame, text="Low-Pass",
+                            command=radiobutton_event, variable=self.filter_select_var, value="lowpass")
+        self.select_lowpass.grid(row=2, column=0, sticky="nsew")
+        self.select_highpass = ctk.CTkRadioButton(self.select_filter_frame, text="High-Pass",
+                            command=radiobutton_event, variable=self.filter_select_var, value="highpass")
+        self.select_highpass.grid(row=3, column=0, sticky="nsew")
+        self.select_bandpass = ctk.CTkRadioButton(self.select_filter_frame, text="Band-Pass",
+                            command=radiobutton_event, variable=self.filter_select_var, value="bandpass")
+        self.select_bandpass.grid(row=4, column=0, sticky="nsew")
 
 
         self.graph_frame = ctk.CTkFrame(self, fg_color="darkred")
         self.graph_frame.grid(row=1, column=0, sticky="nsew")
-
         self.graph_frame.grid_columnconfigure(0, weight=1)
-        self.graph_frame.grid_rowconfigure(0, weight=1)
+        self.graph_frame.grid_rowconfigure((1, 3), weight=1)
 
-        self.wave_graph = ctk.CTkCanvas(self.graph_frame)
-        self.wave_graph.grid(row=0, column=0, sticky="nsew")
+        self.graph_label_freq = ctk.CTkLabel(self.graph_frame, fg_color="grey", text="Frequency Domain", anchor="w", padx=10)
+        self.graph_label_freq.grid(row=0, column=0, sticky="ew")
+        self.freq_domain_graph = ctk.CTkCanvas(self.graph_frame)
+        self.freq_domain_graph.grid(row=1, column=0, sticky="ew")
+        self.graph_label_time = ctk.CTkLabel(self.graph_frame, fg_color="grey", text="Time Domain", anchor="w", padx=10)
+        self.graph_label_time.grid(row=2, column=0, sticky="ew")
+        self.time_domain_graph = ctk.CTkCanvas(self.graph_frame)
+        self.time_domain_graph.grid(row=3, column=0, sticky="ew")
 
 
     def on_close(self):
@@ -145,3 +139,53 @@ class UI(ctk.CTk):
                 messagebox.showerror("Error", f"Could not pause file:\n{e}")
         else:
             messagebox.showwarning("No file", "Please play a file first!")
+
+
+
+
+class FilesFrame(ctk.CTkFrame):
+    def __init__(self, master, height):
+        super().__init__(master)
+
+        self.files_frame = ctk.CTkFrame(master.toolbar_frame, height=height)
+        self.files_frame.grid(row=0, column=0, sticky="nsew")
+        self.files_frame.grid_columnconfigure(0, weight=1)
+        self.files_frame.grid_rowconfigure((0, 1, 2, 3), weight=1)
+
+        file_button_color = "grey"
+        file_button_color_hover = "darkgreen"
+
+        self.close_program_button = ctk.CTkButton(self.files_frame, text="Close Program", command=master.on_close,
+                                           fg_color="darkred", hover_color="red")
+        self.close_program_button.grid(row=0, column=0, sticky="nsew")
+        self.import_button = ctk.CTkButton(self.files_frame, text="Import File", command=master.import_file,
+                                           fg_color=file_button_color, hover_color=file_button_color_hover)
+        self.import_button.grid(row=1, column=0, sticky="nsew")
+        self.export_button = ctk.CTkButton(self.files_frame, text="Export File", command=master.export_file,
+                                           fg_color=file_button_color, hover_color=file_button_color_hover)
+        self.export_button.grid(row=2, column=0, sticky="nsew")
+        self.close_button = ctk.CTkButton(self.files_frame, text="Close File", command=master.export_file,
+                                           fg_color=file_button_color, hover_color=file_button_color_hover)
+        self.close_button.grid(row=3, column=0, sticky="nsew")
+
+class ControlsFrame(ctk.CTkFrame):
+    def __init__(self, master, height):
+        super().__init__(master)
+
+        self.controls_frame = ctk.CTkFrame(master.toolbar_frame, height=height)
+        self.controls_frame.grid(row=0, column=1, sticky="nsew")
+        self.controls_frame.grid_columnconfigure((0, 1), weight=1)
+        self.controls_frame.grid_rowconfigure(0, weight=1)
+
+        control_button_width = 10
+        control_button_color = "grey"
+        control_button_color_hover = "darkgreen"
+
+        self.play_button = ctk.CTkButton(self.controls_frame, text="Play", command=master.play_audio,
+                                         width=control_button_width,
+                                         fg_color=control_button_color, hover_color=control_button_color_hover)
+        self.play_button.grid(row=0, column=0, sticky="nsew")
+        self.pause_button = ctk.CTkButton(self.controls_frame, text="Pause", command=master.pause_audio,
+                                          width=control_button_width,
+                                          fg_color=control_button_color, hover_color=control_button_color_hover)
+        self.pause_button.grid(row=0, column=1, sticky="nsew")
