c858c7dca8798af7348d7ac3939ef53db8084322
nillnought <gglaim@my.bcit.ca>
2025-11-18 08:57:05 -0800

    Implement test versions of domain plots

--- audioFile.py
+++ audioFile.py
@@ -1,6 +1,8 @@
 import soundfile as sf
 import numpy as np
 import sounddevice as sd
+import matplotlib as plt
+from scipy.fft import fft, fftfreq
 class audioFile:
 
 
@@ -9,6 +11,8 @@ class audioFile:
         self.file_address = file_location
         self.audio_data, self.samp_freq = sf.read(file_location)
         self.playing = False
+        audio_file = sf.SoundFile(file_location)
+        self.duration = audio_file.frames / audio_file.samplerate
 
 # usage: audio.saveFile("newName.wav") can look into appending a file format when user chooses to save
     def saveFile(self, new_file_name):
@@ -25,3 +29,19 @@ class audioFile:
         if(self.playing):
             #pause logic
             print("pause")
+
+#need to test
+    def drawAmpDomain(self):
+        plt.figure(figsize=(10, 5))
+        plt.plot(self.duration, self.audio_data)
+        plt.show()
+
+#need to test
+    def drawFreqDomain(self):
+        N = len(self.audio_data)
+        yf = fft(self.audio_data)
+        xf = fftfreq(N, 1/self.samp_freq)[:N // 2]
+
+        plt.figure(figsize=(10,5))
+        plt.plot(xf, np.abs(yf[:N //2]))
+        plt.show()
