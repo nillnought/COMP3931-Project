968d4e287d7c250d27c1a88dd2c004f5965657ef
nillnought <gglaim@my.bcit.ca>
2025-11-18 12:07:26 -0800

    remove unecessary code

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 # runs the whole application
 
-import tkinter as tk
 from UI import UI
 
 def main():
-    # root = tk.Tk()
-    # app = UI(root)
-    # root.mainloop()
     app = UI()
     app.mainloop()
 
