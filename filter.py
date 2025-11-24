7364e992b9b9c4917f44674f735c36695918015b
nillnought <gglaim@my.bcit.ca>
2025-11-24 15:22:58 -0800

    fix and check functionality of all filters

--- filter.py
+++ filter.py
@@ -3,22 +3,33 @@ from scipy.fft import fft, ifft, fftfreq
 import numpy as np
 
 
-def reverb(audio, samp_freq, amp_reduction=0.5, delay_ms=50):
+def reverb(audio, samp_freq, amp_reduction=0.5, delay_ms=50, repeats =8):
     #applies reverb
     delay_samps = int(samp_freq * delay_ms / 1000)
-    impulse = np.zeros(delay_samps * 8)
-    for i in range(8):
+    impulse = np.zeros(delay_samps * repeats +1)
+    for i in range(repeats):
         impulse[i * delay_samps] = amp_reduction ** i
+    impulse[0] = 1.0
 
-    return(sg.fftconvolve(audio, impulse, mode="same"))
+    if audio.ndim == 1:
+        return sg.fftconvolve(audio, impulse, mode="full")
+    else:
+        filtered = sg.fftconvolve(audio, impulse[:, None], mode="full", axes=0)
+        return filtered
 
-def echo(audio, samp_freq, delay_ms=300, decay=0.6):
+
+def echo(audio, samp_freq, delay_ms=300, decay=0.3, repeats = 5):
     #applies echo
     delay_samps = int(samp_freq*delay_ms /1000)
-    impulse = np.zeros(delay_samps + 1)
+    impulse = np.zeros(delay_samps*repeats + 1)
+    for i in range(repeats):
+        impulse[i * delay_samps] = decay ** i
     impulse[0] = 1.0
-    impulse[-1] = decay
-    return(sg.fftconvolve(audio, impulse, mode="same"))
+    if audio.ndim == 1:
+        return(sg.fftconvolve(audio, impulse, mode="full"))
+    else:
+        filtered_data = sg.fftconvolve(audio, impulse[:, None], mode="full", axes=0)
+        return filtered_data
 
 #need to test
 def lowPass(audio, samp_freq, cutoff=20):
@@ -48,7 +59,7 @@ def bandPass(audio, samp_freq, high=50, low=20):
     #keep freqs within certain range
     N = audio.shape[0]
     freqs = fftfreq(N, 1/samp_freq)
-    mask = (np.abs(freqs) >= low) & (np.abs(freqs) <= high)
+    mask = ((np.abs(freqs) >= low) & (np.abs(freqs) <= high))
     fft_data = fft(audio, axis=0)
     filtered_audio = ifft(fft_data * mask[:, None], axis=0)
 
--- main.py
+++ main.py
@@ -2,13 +2,28 @@
 
 from UI import UI
 from audioFile import audioFile
+from filter import highPass, lowPass, bandPass, echo, reverb
 
 def main():
-    app = UI()
-    app.mainloop()
-    # audio = audioFile("test.wav")
-    # audio.drawAmpDomain()
-    # audio.drawFreqDomain()
+    # app = UI()
+    # app.mainloop()
+    audio = audioFile("test.wav")
+    audio.playSound()
+    # YAY LOW PASS WORKS!!!
+    # audio.audio_data = lowPass(audio.audio_data, audio.samp_freq, 600)
+    # audio.playSound()
+    # YAY HIGH PASS WORKS!!!
+    # audio.audio_data = highPass(audio.audio_data, audio.samp_freq, 300)
+    # audio.playSound()
+    # YAY BANDPASS WORKS!!!
+    # audio.audio_data = bandPass(audio.audio_data, audio.samp_freq, 600, 300)
+    # audio.playSound()
+    # YAY ECHO WORKS!!
+    # audio.audio_data = echo(audio.audio_data, audio.samp_freq)
+    # audio.playSound()
+    # YAY REVERB WORKS!!
+    # audio.audio_data = reverb(audio.audio_data, audio.samp_freq)
+    # audio.playSound()
 
 if __name__ == "__main__":
     main()
